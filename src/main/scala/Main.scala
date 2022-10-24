ac067e713ab92ec60099442f60589b5c06560585
jobdk <johnybudnik+git@gmail.com>
2022-10-24 13:42:52 +0200

    finished

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,9 @@
-package services
-
 import Utils.SqlCommands.{ADD_ALL_KEYS, DROP_ALL_KEYS}
 import Utils.TimeUtils.{getCurrentTime, getDuration}
 import services.BatchService.startReading
 import services.DatabaseService.runSqlCommand
 
-object Batch {
+object Main {
 //    private val fileName = "small.json"
   private val fileName = "dblp.v12.json"
   def main(args: Array[String]): Unit = {
